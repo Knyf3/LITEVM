4aff80ba4d622ad5eb12cf817b685d65dc5fdf47
Fenky <fenky.tjhang@gmail.com>
2026-06-22 15:37:10 +0700

    Wire up Apps Script Web App URL in config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
  * Update API_BASE with your Google Apps Script Web App URL during deployment.
  */
 const CONFIG = {
-  API_BASE: '',  // e.g. 'https://script.google.com/macros/s/.../exec'
+  API_BASE: 'https://script.google.com/macros/s/AKfycbyQA6WibRYfpTJYA7syYaskM2n45csIs_sjzn-FfF8sNKaAFWOkIrNcRfYC-nTJc7JK/exec',
   SITE_NAME: 'Visitor Registration',
   PHOTO_MAX_SIZE: 5 * 1024 * 1024,  // 5MB
   PHOTO_MAX_DIMENSION: 1024,  // max pixels on longest edge
